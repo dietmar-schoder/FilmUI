e4a90335a93cf83162b406fc1bb7dfd554416398
danielschoder <daniel@schoder.uk>
2025-06-23 19:27:54 +0100

    new

---
+++ FilmUI/Shared/ListPageBase.cs
@@ -0,0 +1,19 @@
+﻿using FilmUI.Helpers;
+using Microsoft.AspNetCore.Components;
+
+namespace FilmUI.Shared;
+
+public abstract class ListPageBase<TDto> : ComponentBase
+{
+    protected List<TDto> Items;
+
+    [Inject]
+    public IApiService Api { get; set; }
+
+    protected abstract string ApiEndpoint { get; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Items = await Api.GetListAsync<TDto>(ApiEndpoint);
+    }
+}
