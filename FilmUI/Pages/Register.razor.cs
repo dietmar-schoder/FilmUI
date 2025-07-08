7300855cdf5c884a5a65edfb00016b33464d072e
danielschoder <daniel@schoder.uk>
2025-07-08 21:11:51 +0100

    r

---
+++ FilmUI/Pages/Register.razor.cs
@@ -0,0 +1,22 @@
+﻿using FilmUI.DTOs;
+using FilmUI.Helpers;
+using Microsoft.AspNetCore.Components;
+
+namespace FilmUI.Pages;
+
+public partial class Register
+{
+    [Inject] protected IApiService Api { get; set; }
+    [Inject] protected NavigationManager Nav { get; set; }
+
+    private readonly LoginDto registerDto = new();
+
+    private async Task RegisterUser()
+    {
+        await Api.PostAsync("/api/users/register", registerDto);
+        if (true)
+        {
+            Nav.NavigateTo("/login");
+        }
+    }
+}
